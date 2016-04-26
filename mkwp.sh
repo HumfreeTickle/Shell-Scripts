48fa4891e691419f4c5cde2b54efea5a6709271a
Stephen Larkin <stephenlarkin64@gmail.com>
2016-04-26 09:13:20 +0100

    First Commit

---
+++ mkwp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Takes in a project name from the user
+echo Enter Project Name:
+read name
+
+echo Creating project directory…
+#Verbal response from the computer (mostly just for fun)
+say -v "Victoria" Creating project directory…
+
+#creates the directory and all sub-directories
+mkdir -p ~/Documents/Website/$name/{design,wireframes_mockups/{wireframe,mockups/{high,low}},images,code/{js/angular,css/sass/{modules,partials,vendors}}}
+
+#add all the starting files like index.html to the correct folders
+cd ~/Documents/Website/$name/code
+touch index.html
+cd js
+touch main.js
+cd ..
+cd css
+touch main.css
+cd sass
+touch main.scss
+
+#informs the user that the project is finished being created
+echo $name project directory completed sir
+say -v "Victoria" $name Project directory completed sir
+
+#opens folder in finder
+open -a Finder ~/Documents/Website/$name
+
+#opens all the code files within Sublime Text
+open -a "Sublime Text" ~/Documents/Website/$name/code
+
+#opens google and searches for images related to project 
+open "http://google.com/images?q=$name+website+design+ideas"
+open "http://google.com/images?q=$name+inspiration+design+ideas"
